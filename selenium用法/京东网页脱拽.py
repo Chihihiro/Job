f1bd7d79a1f6be4a3e90534f9e91f84fb966d535
Chihiro <632207812@qq.com>
2019-04-29 18:53:37 +0800

    111

--- selenium用法/京东网页脱拽.py
+++ selenium用法/京东网页脱拽.py
@@ -16,7 +16,11 @@ import csv
 
 
 # 京东首页
-jd_url = "https://www.jd.com/"
+danci = '古筝'
+jd_url = f"https://search.jd.com/Search?keyword={request.quote(danci)}&enc=utf-8&spm=a.0.0&pvid=60aa147f26a44161b4f3491cf8ab5ca1"
+"https://search.jd.com/Search?keyword=%E5%8F%A4%E7%AD%9D&enc=utf-8&wq=%E5%8F%A4%E7%AD%9D&pvid=baa8b0181aea45d387af9e6ca2273ddd"
+"https://search.jd.com/Search?keyword=%E5%8F%A4%E7%AD%9D&enc=utf-8&qrst=1&rt=1&stop=1&spm=a.0.0&vt=2&page=5&s=109&click=0"
+# jd_url = 'https://www.jd.com/'
 
 
 def get_html():
@@ -28,9 +32,11 @@ def get_html():
     browser.get(jd_url)
     sleep(3)
 
-    for i in range(0, 0000, 5):
+    for i in range(0, 10000, 5):
         # 拖动网页的js代码
         js = f"window.scroll(0,{i})"
+        print(i)
+        sleep(0.001)
         # 执行js脚本
         browser.execute_script(js)
 
@@ -49,21 +55,28 @@ def parse():
         content = file.read()
 
     selector = Selector(text=content)
-    shop_list = selector.css("#J_more ul li")
+    # shop_list = selector.css("#J_more ul li")
+    shop_list = selector.xpath('//*[@id="J_goodsList"]/ul/li')
+    print(shop_list)
 
     csv_file = open("京东商品信息.csv", "w", encoding="gbk", newline="")
     writer = csv.writer(csv_file)
     for i, each in enumerate(shop_list):
-        name = each.css("a::attr(title)").extract()
-
-        price = each.css("span::text").extract()
+        # name = each.css("a::attr(title)").extract()
+        i = i+1
+        name = each.xpath(f"//li[{i}]/div/div[3]/a/em/text()").extract()
+        print(len(name))
+        print(name)
+        # price = each.css("span::text").extract()
+        price = each.xpath(f"//li[{i}]/div/div[2]/strong/i/text()").extract()
+        print(price)
         if name and price:
-            name = name[0]
+            name = "".join(name)
             price = price[0]
             value = [name, price]
             writer.writerow(value)
     csv_file.close()
 
-
+get_html()
 # parse()
 
